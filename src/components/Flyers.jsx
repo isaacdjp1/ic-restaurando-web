1fe667372ac28a3cba18f406acc33b4c0fdc5eb0
isaacdjp1 <isaacjackson286@gmail.com>
2026-05-16 13:14:07 -0400

    Update flyers with new event details and images

--- src/components/Flyers.jsx
+++ src/components/Flyers.jsx
@@ -5,40 +5,41 @@ import { Autoplay } from "swiper/modules"
 
 import "swiper/css"
 
-import ViernesImg from "../assets/images/Viernes.webp"
-import SabadoImg from "../assets/images/Eventodamas.webp"
-import DomingoImg from "../assets/images/Domingo.webp"
+import ViernesImg from "../assets/images/Eventodamas.webp"
+import SabadoImg from "../assets/images/Domingo.webp"
+import DomingoImg from "../assets/images/Martes.webp"
 
 const flyers = [
-  {
-    title: "Servicio Congregacional",
-    day: "Viernes",
-    time: "7:00 PM - 9:00 PM",
-
-    description:
-      "Cada viernes vivimos un tiempo de adoración, palabra y ministración donde buscamos la presencia de Dios juntos como iglesia.",
-
-    image: ViernesImg,
-  },
-
   {
     title: "Mujeres Indetenibles",
     day: "Sábado",
     time: "4:30 PM - 6:30 PM",
 
     description:
-      "Un espacio diseñado para que los jóvenes crezcan espiritualmente, hagan amistades y experimenten a Dios de una manera auténtica.",
+      "Un espacio especial donde las mujeres pueden fortalecer su fe, compartir experiencias, crecer espiritualmente y descubrir el propósito de Dios para sus vidas en una comunidad llena de amor y apoyo.”
 
-    image: SabadoImg,
+    image: ViernesImg,
   },
 
   {
     title: "Escuela Dominical",
     day: "Domingo",
-    time: "9:00 AM - 12:00 PM",
+    time: "8:00 AM - 11:30 PM",
 
     description:
       "Nuestra reunión principal de domingo donde toda la familia se reúne para adorar, aprender y compartir juntos.",
+
+    image: SabadoImg,
+  },
+
+  {
+    title: "Escuela de lideres",
+    day: "Martes",
+    time: "7:00 PM - 8:30 PM",
+
+    description:
+      "Un tiempo de formación y crecimiento espiritual diseñado para preparar líderes comprometidos con el llamado de Dios. Cada encuentro es una oportunidad para aprender, servir y fortalecer el propósito ministerial en una atmósfera de fe y enseñanza.”
+
 
     image: DomingoImg,
   },
